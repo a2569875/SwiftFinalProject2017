4d6649858893687c0deb554faadcf4127225ff5b
張宇凡 <a2569875@zhangyufans-Mac.local>
2018-01-15 00:15:02 +0800

    加入選單

---
+++ FinalProject00357123/SelectMyCharactorCell.swift
@@ -0,0 +1,24 @@
+//
+//  SelectMyCharactorCell.swift
+//  FinalProject00357123
+//
+//  Created by 張宇帆 on 2018/1/15.
+//  Copyright © 2018年 NTOU department of computer science. All rights reserved.
+//
+
+import UIKit
+
+class SelectMyCharactorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
