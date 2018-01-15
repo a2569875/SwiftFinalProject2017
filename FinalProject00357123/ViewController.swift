f885eef0141662d31d1d200155fe9845d5684550
張宇凡 <a2569875@zhangyufans-Mac.local>
2018-01-15 20:59:35 +0800

    類別Global化

--- FinalProject00357123/ViewController.swift
+++ FinalProject00357123/ViewController.swift
@@ -15,42 +15,6 @@ import GameplayKit
 
 class ViewController: GLKViewController {
 
-    struct PropertyKeys {
-        static let BodyAngleX = "PARAM_BODY_ANGLE_X"
-        static let BodyAngleY = "PARAM_BODY_ANGLE_Y"
-        static let BodyAngleZ = "PARAM_BODY_ANGLE_Z"
-        
-        static let AngleX = "PARAM_ANGLE_X"
-        static let AngleY = "PARAM_ANGLE_Y"
-        static let AngleZ = "PARAM_ANGLE_Z"
-        
-        static let BrowLeftX = "PARAM_BROW_L_X"
-        static let BrowRightX = "PARAM_BROW_R_X"
-        static let BrowLeftY = "PARAM_BROW_L_Y"
-        static let BrowRightY = "PARAM_BROW_R_Y"
-        static let BrowLeftAngle = "PARAM_BROW_L_ANGLE"
-        static let BrowRightAngle = "PARAM_BROW_R_ANGLE"
-        
-        static let LeftEyeOpen = "PARAM_EYE_L_OPEN"
-        static let RightEyeOpen = "PARAM_EYE_R_OPEN"
-        
-        static let MouseOpenX = "PARAM_MOUTH_OPEN_X"
-        static let MouseOpenY = "PARAM_MOUTH_OPEN_Y"
-        static let MouseForm = "PARAM_MOUTH_FORM"
-        
-        static let HairFront = "PARAM_HAIR_FRONT"
-        static let HairBack = "PARAM_HAIR_BACK"
-        
-        static let ModuleBreath = "PARAM_BREATH"
-        
-        static let ModuleBustX = "PARAM_BUST_X"
-        static let ModuleBustY = "PARAM_BUST_Y"
-        
-        static let ModuleFirstArmLeftA1 = "PARTS_01_ARM_L_A_001"
-        static let ModuleFirstArmRightA1 = "PARTS_01_ARM_R_A_001"
-        //static let editAthleteSegue = "EditAthlete"
-    }
-    
     var loadded_setting : GobalSetting!
     var modelFile = "Nanachi"
     let contentUpdater = ContentUpdater()
